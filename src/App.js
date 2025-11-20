2b498574f55c282c4e1154371d293e3ba6c7c15c
Rakil Kim <rakilkim0216@gmail.com>
2025-11-20 14:25:03 -0800

    add background

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CardMobile from './components/CardMobile';
-import QuoteMobile from './components/QuoteMobile';
 import MobileContainer from "./components/MobileContainer";
 
 function App() {
