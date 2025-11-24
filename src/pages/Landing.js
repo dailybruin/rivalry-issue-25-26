f8ad4d4a4fb8719b61ebbbafcc22188950509870
ycho1908 <yehyeoncho@gmail.com>
2025-11-23 21:32:40 -0800

    landing ready for deployment

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,14 @@
 // import landingImg from "../images/landing.png";
-import rivalryLanding from "../images/rivalrylanding.png"
+// import rivalryLanding from "../images/rivalrylanding.png"
 import "./Landing.css";
 
-const Landing = ({landing}) => {
+const Landing = ({data}) => {
+  const creditText = data.landing_credits || "Helen Juwon Park / Illustrations Director & Kaylen Ho / Daily Bruin Staff"
   return (
     <div className="landing-container">
-      <img src={rivalryLanding} alt="Landing illustration" />
+      <img src={data.landing_image} alt="Landing illustration" />
       <div className="credits">
-        <h1>Helen Juwon Park / Illustrations Director & Kaylen Ho / Daily Bruin Staff</h1>
+        <h1>{creditText}</h1>
       </div>
     </div>
   );
