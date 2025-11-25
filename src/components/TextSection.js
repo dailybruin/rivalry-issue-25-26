df7d1c281fd0a863fa0d3da332627f40b4c80b10
ycho1908 <yehyeoncho@gmail.com>
2025-11-25 00:59:01 -0800

    data appearing fine, but off formatting

--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -1,10 +1,13 @@
 import "./TextSection.css"
 
 const TextSection = ({text}) => {
+    // Handle case where text might be an object or null/undefined
+    const displayText = typeof text === 'object' && text?.text ? text.text : (text || '');
+    
     return (
         <div className="text-section">
             <div className="text-box">
-                <p>{text}</p>    
+                <p>{displayText}</p>    
             </div>
         </div>
     );
